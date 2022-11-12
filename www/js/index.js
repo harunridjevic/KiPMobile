56cfd026cfd3437da538fe459b155c62ebfc2e35
mrharryridd <114097440+mrharryridd@users.noreply.github.com>
2022-11-13 00:45:33 +0100

    v0.1.4
    New features:
    -minimize navigation when scrolling
    -added in a bottom navigation div
    -added effects on category buttons

--- www/js/index.js
+++ www/js/index.js
@@ -26,12 +26,14 @@ navbar.style.cursor = 'pointer';
 navbar.onclick = function() {
 	if(dropdown == 0){
    navbar.classList.add("dropdown");
+   document.getElementById("nav").style.height = "100%";
    arrow.classList.remove("arrow-normal");
    arrow.classList.add("arrow-change");
    navcontent.classList.remove("invisible");
    dropdown++;
 	}else{
 		navbar.classList.remove("dropdown");
+		document.getElementById("nav").style.height = "60px";
 		arrow.classList.remove("arrow-change");
 		arrow.classList.add("arrow-normal");
 		navcontent.classList.add("invisible");
